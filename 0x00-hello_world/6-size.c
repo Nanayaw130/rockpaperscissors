136849c5b578672346c2053e61dca5c8a18d6f99
Nanayaw130 <gyeduamankrah@gmail.com>
2022-09-09 11:53:14 +0000

    mman

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 
-int main(){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) {
 char charType;
 int intType;
 long longType;
@@ -8,15 +13,17 @@ long long  longlongType;
 float floatType;
 
 
-// sizeof evaluates the size of a variable
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long int: %zu byte\n", sizeof(longType));
-printf("Size of long long int: %zu byte\n", sizeof(longlongType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
+/* The code below will print the size of the various
+*data
+*/
+printf("Size of char: %zu bytes\n", sizeof(charType))(s);
+printf("Size of int: %zu bytes\n", sizeof(intType))(s);
+printf("Size of long int: %zu bytes\n", sizeof(longType))(s)
+printf("Size of a long long int: %zu byte\n", sizeof(longlongType))(s);
+printf("Size of a float: %zu bytes\n", sizeof(floatType))(s);
 
 
-   return 0;
+return (0);
 }
 
 
