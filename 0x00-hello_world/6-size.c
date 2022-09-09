e9734157792ac366c8f8dcee4a985123f4996483
Nanayaw130 <gyeduamankrah@gmail.com>
2022-09-09 11:58:25 +0000

    nana

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ float floatType;
 /* The code below will print the size of the various
 *data
 */
-printf("Size of char: %zu bytes\n", sizeof(charType))(s);
-printf("Size of int: %zu bytes\n", sizeof(intType))(s);
-printf("Size of long int: %zu bytes\n", sizeof(longType))(s)
-printf("Size of a long long int: %zu byte\n", sizeof(longlongType))(s);
-printf("Size of a float: %zu bytes\n", sizeof(floatType))(s);
+printf("Size of char: %zu bytes\n", sizeof(charType(s));
+printf("Size of int: %zu bytes\n", sizeof(intTypes(s));
+printf("Size of long int: %zu bytes\n", sizeof(longType(s));
+printf("Size of a long long int: %zu byte\n", sizeof(longlongType(s));
+printf("Size of a float: %zu bytes\n", sizeof(floatType(s));
 
 
 return (0);
